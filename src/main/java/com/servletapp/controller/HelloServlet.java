97cab71f0436c858105591cf2c602bf027e27df9
bennyp11 <ben.r.pollock@gmail.com>
2023-02-17 12:54:30 -0800

    first commit of servlet doing Server side rendering

---
+++ src/main/java/com/servletapp/controller/HelloServlet.java
@@ -0,0 +1,35 @@
+package com.servletapp.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("WELCOME TO YOUR HELLO SERVLET");	
+		
+		response.setContentType("text/html");
+		String htmlResponse = "<center><h1>WELCOME TO YOUR SERVLET, DAWG</h1></center>";
+		
+		PrintWriter out = response.getWriter();
+		out.print(htmlResponse);
+	}
+
+}
